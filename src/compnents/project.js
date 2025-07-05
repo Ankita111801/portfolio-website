502cc5fcc6ba870a3ef1e9ff657cfc7109771cb3
Ankita111801 <ankitarai737@gmail.com>
2025-07-05 19:03:06 +0530

    Create project.js

---
+++ src/compnents/project.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Projects.css";
+
+function Projects() {
+  return (
+    <section className="projects">
+      <h2>Projects</h2>
+      <ul>
+        <li><a href="https://github.com/Ankita111801/portfolio-website">Portfolio Website</a></li>
+        <li><a href="https://github.com/Ankita111801/chat-app">Chat App</a></li>
+        <li><a href="https://github.com/Ankita111801/todo-list">To-Do App</a></li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
