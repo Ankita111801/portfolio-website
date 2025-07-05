96759995eb8cda641cf11c8a5e845359e44c88a5
Ankita111801 <ankitarai737@gmail.com>
2025-07-05 18:54:42 +0530

    Update App.js

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,1 +1,18 @@
 // Sample React App.js content
+import React from "react";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
