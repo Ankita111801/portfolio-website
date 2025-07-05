46a8c3ab68ea4fc9f304cca9a58e3a68469de59d
Ankita111801 <ankitarai737@gmail.com>
2025-07-05 19:06:41 +0530

    Create Contact.js

---
+++ components/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <section className="contact">
+      <h2>Contact Me</h2>
+      <p>Email: <a href="mailto:2211701110@stu.manit.ac.in">2211701110@stu.manit.ac.in</a></p>
+      <p>GitHub: <a href="https://github.com/Ankita111801">Ankita111801</a></p>
+    </section>
+  );
+}
+
+export default Contact;
