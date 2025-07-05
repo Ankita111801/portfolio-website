fd18a2eb4793d78f9a9d08c9471f6f654e492512
Ankita111801 <ankitarai737@gmail.com>
2025-07-05 18:59:23 +0530

    Update and rename README.md to Header.js

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Ankita Rai</h1>
+      <p>Frontend Developer | React Enthusiast</p>
+    </header>
+  );
+}
+
+export default Header;
